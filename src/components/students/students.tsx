0ca108cadd9a86a7c118bc9c53d9eb2e3b2f3218
Tommy Egbe <tabe_e@yahoo.ca>
2023-01-27 23:28:09 +0100

    added content for student page

--- src/components/students/students.tsx
+++ src/components/students/students.tsx
@@ -1,9 +1,57 @@
-import React from "react";
-import CommonComponent from '../common-component/common-component';
+import Card from 'react-bootstrap/Card';
+import '../common-component/common-component.css';
 
 const Students = () => {
     return(
-        <CommonComponent />
+        <div className='student-main'>
+            <h1 className='student-text'>Student Admissions</h1>
+            <div className='student-cards'>
+                <Card className='student-card'>
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                        <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+                <Card className='student-card-b'>
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                        <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </div>
+        </div>
         )
 }
 
